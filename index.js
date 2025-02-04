e5d8d068006a2fe91823ae753815602b8741f0c2
Sufhaf <sufyaanhafiji10@gmail.com>
2025-02-04 22:41:07 +0000

    Update index.js

--- index.js
+++ index.js
@@ -1,28 +1,28 @@
-const display = document.getElementById("display");
-const clickSound = new Audio("click.mp3");
+const display = document.getElementById("display"); // Gets the display element from the DOM
+const clickSound = new Audio("click.mp3"); 
 const clearSound = new Audio("reset.wav");
-const enterSound = new Audio("enter.wav");
+const enterSound = new Audio("enter.wav"); // These all load sound files for the calculator
 
 
-function appendToDisplay(input){
-    display.value += input;
-    clickSound.play();
-    clickSound.currentTime = 0;
+function appendToDisplay(input){ // Function to append input to the display
+    display.value += input; //Append the input value to the display
+    clickSound.play(); 
+    clickSound.currentTime = 0; // Plays mp3 file sound on input
 }
 
-function clearDisplay(){
-    display.value ="";
+function clearDisplay(){ // Function to clear the display on click
+    display.value =""; // Sets to value of the display to blank
     clearSound.play();
-    clearSound.currentTime = 0;
+    clearSound.currentTime = 0; // Plays mp3 file sound on input
 }
 
-function calculate(){
-    try{
-    display.value = eval(display.value);
+function calculate(){ // Function to calculate the arithmatic 
+    try{ // Using try, it allows an error input for eronius inputs the user does
+    display.value = eval(display.value);  // Calculates and evaluates the arithmatic operation and displays it
     enterSound.play();
-    enterSound.currentTime = 0;
+    enterSound.currentTime = 0; // Plays mp3 file sound on input
     }
-    catch(error){
+    catch(error){ // If an eronious input has occured, display text 'Error'
         display.value = "Error";
     }
 }
